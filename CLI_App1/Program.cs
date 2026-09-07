700d638fad4beefa4013d1f348b054fc5a2496be
MonZ <aymanzakaria246@gmail.com>
2026-09-08 01:06:25 +0800

    Commit Test

---
+++ CLI_App1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.Green;
+
+Console.Write("Hello ");
+Console.WriteLine("World!");
+Console.Write("Write your name: ");
+string name = Console.ReadLine();
+Console.Write("Good day, ");
+Console.Write(name);
+Console.WriteLine("!");
