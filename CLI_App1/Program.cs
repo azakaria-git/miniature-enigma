c4f7840408879df9c389a023df5ec4054351a6b0
MonZ <aymanzakaria246@gmail.com>
2026-09-10 20:52:40 +0800

    Added selection Menu and null string dealer

--- CLI_App1/Program.cs
+++ CLI_App1/Program.cs
@@ -2,18 +2,76 @@
 using System.Collections.Generic;
 using System.IO;
 
-string PathFile;
+string? PathFile;
+
 Console.WriteLine("CSV Reader");
 
 while (true)
 {
     Console.Write("File Path: ");
     PathFile = Console.ReadLine();
+    // Null Dealing
+    if (PathFile == null)
+    {
+        Console.WriteLine("Invalid. Try again: ");
+    }
 
     // File Validation
-    if (File.Exists(PathFile)) break;
-
-    Console.WriteLine("File not Found!");
+    if (File.Exists(PathFile))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("File not Found! Try again: ");
+    }
 }
 
-Console.Write("File Exists!");
+Console.WriteLine("File Exists!");
+
+// Menu
+Console.Clear();
+Console.WriteLine("Main Menu");
+Console.WriteLine("1. Read File");
+Console.WriteLine("2. Find and Read Columns");
+Console.WriteLine("3. Edit a Cell");
+Console.WriteLine("4. Quit Program");
+MainMenu();
+
+// Functions according to Selection
+static void ReadFile(string FilePath)
+{
+    
+}
+
+static void MainMenu()
+{
+    int selNum;
+    while (true)
+    {
+        Console.Write("Select Option: ");
+        string selection = Console.ReadLine();
+        if (!int.TryParse(selection, out selNum))
+        {
+            Console.Write("Invalid Selection! Try again: ");
+            selection = Console.ReadLine();
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    switch (selNum)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            Console.WriteLine("Exiting Program. Thank You.");
+            break;
+    }
+}
